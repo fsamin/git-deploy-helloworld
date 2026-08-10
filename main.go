e93c34f746d4a1dd394fd48d84889031608d6639
François Samin <francois.samin+github@gmail.com>
2026-08-10 21:53:01 +0200

    Bump version to v3

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // version is bumped on each demo commit to make rolling updates visible.
-const version = "v2"
+const version = "v3"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
