42a88a9dce89af15a115d7417c00c126a6798aaa
François Samin <francois.samin+github@gmail.com>
2026-08-11 08:48:46 +0200

    Bump version to v4

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // version is bumped on each demo commit to make rolling updates visible.
-const version = "v3"
+const version = "v4"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
